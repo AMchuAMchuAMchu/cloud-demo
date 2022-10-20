5cd05ed1ac327a7955b4eb80a1ac2cb866833901
AMchuAMchuAMchu <2466130840@qq.com>
2022-10-20 20:30:53 +0800

    AM 01st commit

--- user-service/src/main/java/cn/itcast/user/web/UserController.java
+++ user-service/src/main/java/cn/itcast/user/web/UserController.java
@@ -4,7 +4,11 @@ import cn.itcast.user.pojo.User;
 import cn.itcast.user.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Slf4j
 @RestController
@@ -13,6 +17,17 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @Value("${pattern.dateformat}")
+    private String dateformat;
+
+    @GetMapping("/now")
+    public String get(){
+        System.out.println(" >>> ");
+        System.out.println(" >>> ");
+        System.out.println(" >>> ");
+        return DateTimeFormatter.ofPattern(dateformat).format(LocalDateTime.now());
+    }
 
     /**
      * 路径： /user/110
