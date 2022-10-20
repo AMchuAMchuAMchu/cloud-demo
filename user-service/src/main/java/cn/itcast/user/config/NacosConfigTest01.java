0a7fcea0b9acdc63778be040e0f630ad8ecc0965
AMchuAMchuAMchu <2466130840@qq.com>
2022-10-20 20:51:55 +0800

    AM 01st commit

---
+++ user-service/src/main/java/cn/itcast/user/config/NacosConfigTest01.java
@@ -0,0 +1,22 @@
+package cn.itcast.user.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * Description ==> TODO
+ * BelongsProject ==> springcloud_basic_first
+ * BelongsPackage ==> cn.itcast.user.config
+ * Version ==> 1.0
+ * CreateTime ==> 2022-10-20 20:36:15
+ * Author ==> _02雪乃赤瞳楪祈校条祭_艾米丽可锦木千束木更七草荠_制作委员会_start
+ */
+@Component
+@ConfigurationProperties(prefix = "pattern")
+@Data
+public class NacosConfigTest01 {
+
+    private String dateformat;
+
+}
--- user-service/src/main/java/cn/itcast/user/web/UserController.java
+++ user-service/src/main/java/cn/itcast/user/web/UserController.java
@@ -1,10 +1,12 @@
 package cn.itcast.user.web;
 
+import cn.itcast.user.config.NacosConfigTest01;
 import cn.itcast.user.pojo.User;
 import cn.itcast.user.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
@@ -13,6 +15,7 @@ import java.time.format.DateTimeFormatter;
 @Slf4j
 @RestController
 @RequestMapping("/user")
+//@RefreshScope
 public class UserController {
 
     @Autowired
@@ -21,12 +24,16 @@ public class UserController {
     @Value("${pattern.dateformat}")
     private String dateformat;
 
+    @Autowired
+    private NacosConfigTest01 nacosConfigTest01;
+
     @GetMapping("/now")
     public String get(){
         System.out.println(" >>> ");
         System.out.println(" >>> ");
         System.out.println(" >>> ");
-        return DateTimeFormatter.ofPattern(dateformat).format(LocalDateTime.now());
+//        return DateTimeFormatter.ofPattern(dateformat).format(LocalDateTime.now());
+        return DateTimeFormatter.ofPattern(nacosConfigTest01.getDateformat()).format(LocalDateTime.now());
     }
 
     /**
